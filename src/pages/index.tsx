d28f8f1133be5fa0019c1d59e98040f976c8aa32
Mohsin Khan <faiz.aks@gmail.com>
2022-01-11 13:47:37 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-mimport Layout from "../components/Layout";
+import Layout from "../components/Layout";
 import BasicMeta from "../components/meta/BasicMeta";
 import OpenGraphMeta from "../components/meta/OpenGraphMeta";
 import TwitterCardMeta from "../components/meta/TwitterCardMeta";
